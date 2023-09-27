a9ab7745aa0eceeadc3a9f39df11522930319a1d
surabhi <surabhi.das@outlook.com>
2023-09-27 10:20:22 -0400

    added user & sql has user table under cafe video database

---
+++ src/main/java/com/inn/cafe/POJO/User.java
@@ -0,0 +1,45 @@
+package com.inn.cafe.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+//uses lombok to generate getters and setters & default constructor
+@Data
+@Entity
+
+// if you have an entity with several columns, but you only set values for a subset of those columns,
+// Hibernate will generate an INSERT statement that includes only the columns with non-null values
+@DynamicInsert
+
+// if you load an entity from the database, make changes to some of its properties, and then save it back
+// to the database, Hibernate will generate an UPDATE statement that includes only the columns that have changed.
+@DynamicUpdate
+@Table(name= "user")
+public class User implements Serializable {
+    private static final long  serialVersionUID= 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="contactNumber")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column (name="status")
+    private String status;
+
+    @Column(name= "role")
+    private String role;
+
+}
